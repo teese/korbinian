b92650248f6e6aeb02708f318886d65278898cbb
Shenger Wang <shenger.wang@tum.de>
2016-09-16 13:01:58 +0000

    parse_uniprot.py edited online with Bitbucket

--- korbinian/prot_list/parse_uniprot/parse_uniprot.py
+++ korbinian/prot_list/parse_uniprot/parse_uniprot.py
@@ -8,7 +8,7 @@ import korbinian
 import korbinian.mtutils as utils
 
 def create_csv_from_uniprot_flatfile(uniprot_flatfile_of_selected_records, n_aa_before_tmd, n_aa_after_tmd, analyse_sp, logging, list_summary_csv_path):
-    logging.info('~~~~~~~~~~~~  starting A03_create_csv_from_uniprot_flatfile   ~~~~~~~~~~~~')
+    logging.info('~~~~~~~~~~~~    starting create_csv_from_uniprot_flatfile     ~~~~~~~~~~~~')
     uniprot_dict_all_proteins = {}
     with open(uniprot_flatfile_of_selected_records, "r") as f:
         records = SwissProt.parse(f)
