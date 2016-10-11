cd282c9a1994d48b079bb4c26e78b1fc3a867bf9
Martin Ortner <martin.ortner@tum.de>
2016-10-11 14:35:32 +0200

    download_homologues_from_simap should work now on win, linux and mac (hopefully)

--- korbinian/simap_download.py
+++ korbinian/simap_download.py
@@ -73,18 +73,20 @@ def download_homologues_from_simap(pathdict, s, logging):
     max_memory_allocation = s["java_max_RAM_memory_allocated_to_simap_download"]
     taxid = s["taxid"]  # eg.'7227' for Drosophila melanogaster
 
-    # code works only on mac!!! reverted to previous version
-    # statvfs = os.statvfs(s["simap_dir"])
-    # available_space = statvfs.f_frsize * statvfs.f_bavail
-    # size = available_space / 1073741824
+    if (str(platform.system()) != 'Windows' and str(platform.system()) != 'Linux'):
+        # code works only on mac!!! reverted to previous version
+        statvfs = os.statvfs(s["simap_dir"])
+        available_space = statvfs.f_frsize * statvfs.f_bavail
+        size = available_space / 1073741824
+        # print initial hard-drive space
+        logging.info('Hard disk remaining space = {:.2f} GB'.format(size))
+    else:
+        byteformat = "GB"
+        data_harddrive = os.path.splitdrive(s["data_dir"])[0]
 
-    byteformat = "GB"
-    data_harddrive = os.path.splitdrive(s["data_dir"])[0]
-
-    # print initial hard-drive space
-    # logging.info('Hard disk remaining space = {:.2f} GB'.format(size))
-    size = utils.get_free_space(data_harddrive, byteformat)
-    logging.info('Hard disk remaining space = {}'.format(size))
+        # print initial hard-drive space
+        size = utils.get_free_space(data_harddrive, byteformat)
+        logging.info('Hard disk remaining space = {}'.format(size))
 
 
     #iterate over each uniprot record contained in the dataframe. note that acc = uniprot accession number
@@ -92,15 +94,17 @@ def download_homologues_from_simap(pathdict, s, logging):
     for acc in df.index:
         # check hand-drive space before each download
         try:
-
-            # MAC only stuff...
-            # statvfs = os.statvfs(s["simap_dir"])
-            # available_space = statvfs.f_frsize * statvfs.f_bavail
-            # size = available_space / 1073741824
-            # if size < 5:
-            size = utils.get_free_space(data_harddrive, byteformat)
-            if size[0] < 5:
-                raise utils.HardDriveSpaceException("Hard drive space limit reached, there is only %s %s space left." % (size[0], size[1]))
+            if (str(platform.system()) != 'Windows' and str(platform.system()) != 'Linux'):
+                # MAC only stuff...
+                statvfs = os.statvfs(s["simap_dir"])
+                available_space = statvfs.f_frsize * statvfs.f_bavail
+                size = available_space / 1073741824
+                if size < 5:
+                    raise utils.HardDriveSpaceException("Hard drive space limit reached, there is only %s %s space left." % (size[0], size[1]))
+            else:
+                size = utils.get_free_space(data_harddrive, byteformat)
+                if size[0] < 5:
+                    raise utils.HardDriveSpaceException("Hard drive space limit reached, there is only %s %s space left." % (size[0], size[1]))
         except utils.HardDriveSpaceException as e:
             logging.warning(e)
         protein_name = df.loc[acc, 'protein_name']
