4e41617c1437fd05a908ae85043f8320f9ca16ec
Martin Ortner <martin.ortner@tum.de>
2017-01-25 14:09:39 +0100

    #2 changes in run_setup_df_file_locations:
    1. checks dataframe for proteins with X in full sequence, removes them
    
    2. joins all TMD sequences together to one string
        - if list_of_TMD is longer than 1 element -> column 'TMD_seq_joined'
        - if list_of_TMD equals 1 element -> column 'TMD_seq_SP'

--- korbinian/prot_list/prot_list.py
+++ korbinian/prot_list/prot_list.py
@@ -216,21 +216,18 @@ def setup_file_locations_in_df(s, pathdict):
 
     # add a column that holds all joined TMD sequences, drop proteins with 'X' in full sequence
     n = 0
-    i = 0
     sys.stdout.write('\njoining TMD sequences, dropping proteins with "X" in full sequence: \n'), sys.stdout.flush()
-    for acc in df.index:
-        if 'X' in df.loc[acc, 'full_seq']:
-            i += 1
-            df = df.drop(acc)
-            sys.stdout.write('Dropped: {}\n'.format(acc))
-    sys.stdout.write('number of dropped proteins: {}\n\n'.format(i)), sys.stdout.flush()
-
+    list_dropped_acc = []
     for acc in df.loc[df['list_of_TMDs'].notnull()].loc[df['list_of_TMDs'] != 'nan'].index:
         n += 1
         if n % 20 == 0:
             sys.stdout.write('.'), sys.stdout.flush()
             if n % 600 == 0:
                 sys.stdout.write('\n'), sys.stdout.flush()
+        if 'X' in df.loc[acc, 'full_seq']:
+            df = df.drop(acc)
+            list_dropped_acc.append(acc)
+            continue
         list_of_TMDs = ast.literal_eval(df.loc[acc, 'list_of_TMDs'])
         if len(list_of_TMDs) > 1:
             TMD_seq_joined = ''
@@ -243,6 +240,7 @@ def setup_file_locations_in_df(s, pathdict):
             SP_TMD_seq = df.loc[acc, 'TM01_seq']
             if not 'X' in SP_TMD_seq:
                 df.loc[acc, 'TMD_seq_SP'] = SP_TMD_seq
+    sys.stdout.write('\ndropped proteins: {}, n = {}\n'.format(list_dropped_acc, len(list_dropped_acc))), sys.stdout.flush()
     # save to a csv
     df.to_csv(pathdict["list_summary_csv"], sep=",", quoting=csv.QUOTE_NONNUMERIC)
 
