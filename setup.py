872aa513e567ec3e1233fe972df7b1f8ee825505
Mark Teese <mark.teese@tum.de>
2017-02-22 12:53:54 +0100

    added SW_MSA_rand_homologues_Entropy_Hist started on random pairwise alignments for actual "FigX18b_random_AAIMON" figure

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name="korbinian",
       author="Mark Teese",
       packages=find_packages(),
       install_requires=['numpy','matplotlib','pandas', 'biopython'],
-      version="0.2.9")
+      version="0.3.0")
