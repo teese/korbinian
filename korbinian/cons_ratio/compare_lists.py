added graph with old normalisation factors improved normalisation to include the percentage_TMD_region for each protein separately fixed a minor path bug in scampi

korbinian/cons_ratio/compare_lists.py
@@ -871,6 +871,8 @@ def compare_lists (s):
     for prot_list in protein_lists:
         rand_TM = dfv.loc[prot_list, 'rand_TM']
         rand_nonTM = dfv.loc[prot_list, 'rand_nonTM']
+        fraction_TM_residues = df_dict[prot_list]['perc_TMD'].mean()
+        print("prot_list : {}, fraction_TM_residues : {}".format(prot_list, fraction_TM_residues))
         # define the stop and start points for the two sections of graph
         stop_before_rand_TM = (100 - rand_TM*100) - d
         start_after_rand_TM = (100 - rand_TM*100) + d
@@ -887,7 +889,7 @@ def compare_lists (s):
             identity = (100 - obs_changes) / 100
             # go through each identity individually, and convert to norm factor
             for aa_ident in identity:
-                norm_factor.append(korbinian.cons_ratio.norm.calc_AAIMON_aa_prop_norm_factor(aa_ident, rand_TM, rand_nonTM))
+                norm_factor.append(korbinian.cons_ratio.norm.calc_AAIMON_aa_prop_norm_factor(aa_ident, rand_TM, rand_nonTM, fraction_TM_residues))
             norm_factor = np.array(norm_factor)
 
             # stop the label from being duplicated for both sections of graph

korbinian/figs/FigX18b_old_norm_method_for_SW_thesis.py
@@ -8,7 +8,7 @@ import sys
 from multiprocessing import Pool
 
 ##########parameters#############
-list_number = 1
+list_number = 2
 data_dir = r"/Volumes/Musik/Databases"
 data_dir = r"D:\Databases"
 repeat_randomisation = False
@@ -101,8 +101,8 @@ if __name__ == "__main__":
     color_norm_line = "#53A7D5"
     color_norm_factor_line = "k"
     alpha = 1
-    s = 2
-    marker = 'x'
+    s = 7
+    marker = '.'
     linewidths = 0.3
     fontsize = 14
 
@@ -175,8 +175,12 @@ if __name__ == "__main__":
     norm_AAIMONs_old = AAIMON_list_10rep / norm_factor_array_10rep_old
     norm_AAIMONs_new = AAIMON_list_10rep / norm_factor_array_10rep_new
 
+    # SCATTER PLOTS SHOWING RESULTS FROM OLD NORMALISATION METHOD
+    ax.scatter(obs_aa_sub_rate_10rep*100, norm_AAIMONs_old, color=color_norm, s=s, alpha=alpha, marker=marker, linewidths=linewidths, label="after normalisation")
+    #ax.scatter(obs_aa_sub_rate_10rep*100, norm_factor_array_10rep_old, s=1, color="r")
+
     # plot the data after normalisation first (not as important as the orig at the beginning)
-    ax.scatter(obs_aa_sub_rate_10rep*100, norm_AAIMONs_new, color=color_norm, s=s, alpha=alpha, marker=marker, linewidths=linewidths, label="after normalisation")
+    #ax.scatter(obs_aa_sub_rate_10rep*100, norm_AAIMONs_new, color=color_norm, s=s, alpha=alpha, marker=marker, linewidths=linewidths, label="after normalisation")
     # plot the original data now, so it is clearly seen
     ax.scatter(obs_aa_sub_rate_10rep*100, AAIMON_list_10rep, color=color_nonnorm, s=s, alpha=alpha, marker=marker, linewidths=linewidths, label="before normalisation")
 
@@ -185,7 +189,7 @@ if __name__ == "__main__":
     # plot only the first replicate (0: 1600)
     norm_curve_x = obs_aa_sub_rate_10rep[:max_num_positions_mutated] * 100
     norm_curve_y = norm_factor_array_10rep_new[:max_num_positions_mutated]
-    ax.plot(norm_curve_x, norm_curve_y, color=color_norm_factor_line, linewidth=2, label="calculated normalisation factor")
+    #ax.plot(norm_curve_x, norm_curve_y, color=color_norm_factor_line, linewidth=2, label="calculated normalisation factor")
 
 
     ########################################################################################
@@ -193,10 +197,6 @@ if __name__ == "__main__":
     #                        STUFF THAT IS NOT PLOTTED ANYMORE                             #
     #                                                                                      #
     ########################################################################################
-
-    # SCATTER PLOTS SHOWING RESULTS FROM OLD NORMALISATION METHOD
-    #ax.scatter(obs_aa_sub_rate_10rep*100, norm_AAIMONs_old, color="r", s=3, alpha=1, label="after norm., old method")
-    #ax.scatter(obs_aa_sub_rate_10rep*100, norm_factor_array_10rep_old, s=1, color="r")
 
     # POLYNOMIAL FIT TO THE ORIGINAL DATA (NOT SHOWN)
     z = np.polyfit(obs_aa_sub_rate_10rep*100, AAIMON_list_10rep, 13)
@@ -221,23 +221,24 @@ if __name__ == "__main__":
     #                                 AXIS LABELS, ETC                                     #
     #                                                                                      #
     ########################################################################################
-    ax.set_ylim(0.9, 1.3)
+    ax.set_ylim(0.9, 1.1)
     ax.set_ylabel("AAIMON", fontsize=fontsize+2)
     ax.set_xlabel("% observed AA substitutions in full protein", fontsize=fontsize+2)
-    ax.set_xlim(0, 75)
+    ax.set_xlim(0, 60)
 
-    # re-order the legend
-    handles, labels = ax.get_legend_handles_labels()
-    handles = [handles[2], handles[0], handles[1]]
-    labels = [labels[2], labels[0], labels[1]]
-    legend = ax.legend(handles, labels, loc='upper left', frameon=True, scatterpoints=25, fontsize=fontsize)
+    # # re-order the legend
+    # handles, labels = ax.get_legend_handles_labels()
+    # handles = [handles[2], handles[0], handles[1]]
+    # labels = [labels[2], labels[0], labels[1]]
+    # legend = ax.legend(handles, labels, loc='upper left', frameon=True, scatterpoints=25, fontsize=fontsize)
     #legend.legendHandles[0]._sizes = [20]
     #legend.legendHandles[2]._sizes = [20]
+    ax.legend(loc='upper left', frameon=True, scatterpoints=25, fontsize=fontsize)
     ax.tick_params(labelsize=fontsize)
     plt.tight_layout()
 
     # save fig, e.g. "D:\Databases\summaries\01\List01_rand\List01_rand_AAIMON_vs_aa_sub.png"
-    fig.savefig(fig_AAIMON_vs_perc_aa_sub_png, dpi=400)
+    fig.savefig(fig_AAIMON_vs_perc_aa_sub_png, dpi=200)
     fig.savefig(fig_AAIMON_vs_perc_aa_sub_pdf)
 
     # check that the final normalised values are close to 1.0

korbinian/prot_list/prot_list.py
@@ -8,6 +8,7 @@ import numpy as np
 import os
 import pandas as pd
 import sys
+import time
 import unicodedata
 
 
@@ -37,6 +38,7 @@ def prepare_protein_list(s, pathdict, logging):
     SCAMPI_nonTM_path = pathdict['SCAMPI_nonTM']
     n_prot_AFTER_dropping_SCAMPI_nonTM_seqences = 'SCAMPI nonTMD file not found!'
     if os.path.isfile(SCAMPI_nonTM_path):
+        modification_date = time.ctime(os.path.getmtime(SCAMPI_nonTM_path))
         SCAMPI_nonTM_list = []
         with open(SCAMPI_nonTM_path) as source:
             for line in source:
@@ -44,10 +46,11 @@ def prepare_protein_list(s, pathdict, logging):
                 SCAMPI_nonTM_list.append(line)
         df = df.drop(SCAMPI_nonTM_list, axis=0)
         n_prot_AFTER_dropping_SCAMPI_nonTM_seqences = df.shape[0]
+    else:
+        modification_date = None
 
     if s['use_scampi_data']:
         df = korbinian.cons_ratio.SCAMPI.read_scampi_data(pathdict, s, logging, df)
-
 
     if "uniprot_entry_name" in df.columns:
         # join the accession and entry name to create a "protein name" for naming files
@@ -274,7 +277,7 @@ def prepare_protein_list(s, pathdict, logging):
     list_acc_X_in_seq = []
     list_acc_missing_TM_indices = []
     for n, acc in enumerate(df.index):
-        if n % 20 == 0:
+        if n % 20 == 0 and n != 0:
             sys.stdout.write('.'), sys.stdout.flush()
             if n % 600 == 0:
                 sys.stdout.write('\n'), sys.stdout.flush()
@@ -392,6 +395,8 @@ def prepare_protein_list(s, pathdict, logging):
     logging.info('\nn_initial_prot: {}'.format(n_initial_prot))
 
     logging.info('n_prot_AFTER_dropping_SCAMPI_nonTM_seqences: {}'.format(n_prot_AFTER_dropping_SCAMPI_nonTM_seqences))
+    if modification_date is not None:
+        logging.info("modification_date of scampi file: {}".format(modification_date))
 
     logging.info('n_prot_AFTER_dropping_without_list_TMDs: {}'.format(n_prot_AFTER_dropping_without_list_TMDs)) # line 107
 
@@ -424,9 +429,11 @@ def prepare_protein_list(s, pathdict, logging):
     #                      calculate random TM and nonTM identity                          #
     #                                                                                      #
     ########################################################################################
-    logging.info("calculating rand_TM and rand_nonTM...")
-    calc_randTM_and_randnonTM(s, pathdict, seq_len=1000, number_seq=1000)
-
+    # only calculate the rand_TM and rand_nonTM using the quick method (1000x1000)
+    # if calc_accurate_random_identity is not True, as a more accurate version will be calculated next
+    if not s["calc_accurate_random_identity"]:
+        logging.info("calculating rand_TM and rand_nonTM using quick method (1000x1000")
+        calc_randTM_and_randnonTM(s, pathdict, logging, seq_len=1000, number_seq=1000)
     logging.info('~~~~~~~~~~~~                     finished prepare_protein_list                      ~~~~~~~~~~~~')
 
 def get_indices_TMD_plus_surr_for_summary_file(dfsumm, TMD, n_aa_before_tmd, n_aa_after_tmd):
@@ -485,7 +492,8 @@ def find_indices_longer_than_prot_seq(df, TMD):
     """
     return df['%s_end_plus_surr'%TMD] > df['seqlen']
 
-def calc_randTM_and_randnonTM(s, pathdict, seq_len, number_seq, multiprocessing_mode=False):
+def calc_randTM_and_randnonTM(s, pathdict, logging, seq_len, number_seq, multiprocessing_mode=False):
+    logging.info('~~~~~~~~~~~~                    starting calc_randTM_and_randnonTM                  ~~~~~~~~~~~~')
     ########################################################################################
     #                                                                                      #
     #                      calculate random TM and nonTM identity                          #
@@ -506,13 +514,13 @@ def calc_randTM_and_randnonTM(s, pathdict, seq_len, number_seq, multiprocessing_
         d_list = [d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d]
         with Pool(processes = s["multiprocessing_cores"]) as pool:
             return_statement_list = pool.map(calc_random_aa_ident_multiprocessing, d_list)
-            output_ser = return_statement_list[0][1]
-            random_aa_identity_list = [return_statement[0] for return_statement in return_statement_list]
-            random_aa_identity = np.array(random_aa_identity_list).mean()
-            sys.stdout.write("\nrandom_aa_identity : {}\n, random_aa_identity_list : {}".format(random_aa_identity, random_aa_identity_list))
-            output_ser["random_sequence_identity_output"] = random_aa_identity
-            # save the series as csv file
-            output_ser.to_csv(rand_ident_TM_csv, sep="\t")
+        output_ser = return_statement_list[0][1]
+        random_aa_identity_list = [return_statement[0] for return_statement in return_statement_list]
+        random_aa_identity = np.array(random_aa_identity_list).mean()
+        sys.stdout.write("\nrandom_aa_identity : {}\n, random_aa_identity_list : {}".format(random_aa_identity, random_aa_identity_list))
+        output_ser["random_sequence_identity_output"] = random_aa_identity
+        # save the series as csv file
+        output_ser.to_csv(rand_ident_TM_csv, sep="\t")
     else:
         # calculate random aa identity based on aa propensity
         korbinian.MSA_normalisation.calc_random_aa_ident(aa_prop_csv_out_TM, rand_ident_TM_csv, seq_len=seq_len, number_seq=number_seq, ident=ident)
@@ -530,18 +538,19 @@ def calc_randTM_and_randnonTM(s, pathdict, seq_len, number_seq, multiprocessing_
         d_list = [d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d,d]
         with Pool(processes = s["multiprocessing_cores"]) as pool:
             return_statement_list = pool.map(calc_random_aa_ident_multiprocessing, d_list)
-            output_ser = return_statement_list[0][1]
-            random_aa_identity_list = [return_statement[0] for return_statement in return_statement_list]
-            random_aa_identity = np.array(random_aa_identity_list).mean()
-            sys.stdout.write("random_aa_identity, {} random_aa_identity_list {}".format(random_aa_identity, random_aa_identity_list))
-            output_ser["random_sequence_identity_output"] = random_aa_identity
-            # save the series as csv file
-            output_ser.to_csv(rand_ident_nonTM_csv, sep="\t")
+        output_ser = return_statement_list[0][1]
+        random_aa_identity_list = [return_statement[0] for return_statement in return_statement_list]
+        random_aa_identity = np.array(random_aa_identity_list).mean()
+        sys.stdout.write("random_aa_identity, {} \nrandom_aa_identity_list {}".format(random_aa_identity, random_aa_identity_list))
+        output_ser["random_sequence_identity_output"] = random_aa_identity
+        # save the series as csv file
+        output_ser.to_csv(rand_ident_nonTM_csv, sep="\t")
     else:
         # calculate random aa identity based on aa propensity
         korbinian.MSA_normalisation.calc_random_aa_ident(aa_prop_csv_out_nonTM, rand_ident_nonTM_csv, seq_len=seq_len, number_seq=number_seq, ident=ident)
     # remove temp file with aa propensity (it is also saved in the rand_ident csv)
     #os.remove(aa_prop_csv_out_nonTM)
+    logging.info('~~~~~~~~~~~~                    finished calc_randTM_and_randnonTM                  ~~~~~~~~~~~~')
 
 def calc_random_aa_ident_multiprocessing(d):
     """ Runs calc_random_aa_ident using for multiprocessing.